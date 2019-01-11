64c9b6b8160a1b3631ece3ff2e5ffa9b537d68ff
Brandon Tsai <brandon@darumatic.com>
2019-01-11 11:20:48 +1100

    refine check_harbor.sh

--- ansible/roles/harbor/files/check_harbor.sh
+++ ansible/roles/harbor/files/check_harbor.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-healthy_container_count = `docker-compose -f /opt/harbor/docker-compose.yml ps | grep Up | wc -l`
+healthy_harbor_count=`docker-compose -f /opt/harbor/docker-compose.yml ps | grep Up | wc -l`
+healthy_postgresql_count=`docker-compose -f /opt/postgresql/docker-compose.yml ps | grep Up | wc -l`
 
-if [ $healthy_count == 9 ] ; then
+if [ $healthy_harbor_count == 9 ] && [ $healthy_postgresql_count == 1 ] ; then
     exit 0
 else
     exit 1

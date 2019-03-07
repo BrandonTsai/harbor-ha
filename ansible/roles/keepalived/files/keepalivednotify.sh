c1c0b8ff83b5f98ef6e1036d99ca1bd9bd188221
Brandon Tsai <brandon@darumatic.com>
2019-03-07 23:08:52 +1100

    Try do not run install.sh again when handover

--- ansible/roles/keepalived/files/keepalivednotify.sh
+++ ansible/roles/keepalived/files/keepalivednotify.sh
@@ -5,25 +5,39 @@ TYPE=$1
 NAME=$2
 STATE=$3
 
+make_sure_postgresql_running(){
+  pushd /opt/postgresql/
+  if [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q postgresql)` ]; then
+    echo "Postgresql is not running."
+    docker-compose postgresql up -d
+    sleep 3
+  else
+    echo "Postgresql is running."
+  fi
+  popd
+
+}
+
 case $STATE in
     "MASTER")
         echo "MASTER" | tee /tmp/keepalived_state
         pushd /opt/postgresql/
+        make_sure_postgresql_running
         docker-compose exec postgresql touch /tmp/touch_me_to_promote_to_me_master
         docker-compose exec postgresql ls /tmp/touch_me_to_promote_to_me_master
         popd
         sleep 5
-        pushd /opt/harbor/
-        /opt/harbor/install.sh --with-clair --with-chartmuseum
-        popd
+        # pushd /opt/harbor/
+        # /opt/harbor/install.sh --with-clair --with-chartmuseum
+        # popd
         exit 0
         ;;
     "BACKUP")
         echo "BACKUP" | tee /tmp/keepalived_state
         sleep 60
-        pushd /opt/harbor/
-        /opt/harbor/install.sh --with-clair --with-chartmuseum
-        popd
+        # pushd /opt/harbor/
+        # /opt/harbor/install.sh --with-clair --with-chartmuseum
+        # popd
         exit 0
         ;;
     "FAULT")

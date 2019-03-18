4302dcaa5b93cf57eca7024ca0ed5a5615c85dff
Brandon Tsai <brandon@darumatic.com>
2019-03-18 19:19:13 +1100

    Support become_standby.sh

--- ansible/roles/keepalived/files/keepalivednotify.sh
+++ ansible/roles/keepalived/files/keepalivednotify.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
 exec &>> /var/log/keepalive_notify.log
 
+echo "---------------------------------------------------------------------"
+date
+
 TYPE=$1
 NAME=$2
 STATE=$3
+KEEPALIVED_CONF="/etc/keepalived/keepalived.conf"
 
 make_sure_postgresql_running(){
   pushd /opt/postgresql/
   if [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q postgresql)` ]; then
     echo "Postgresql is not running."
-    docker-compose postgresql up -d
+    docker-compose up -d postgresql
     sleep 3
   else
     echo "Postgresql is running."
@@ -18,35 +22,50 @@ make_sure_postgresql_running(){
 
 }
 
+become_master(){
+  pushd /opt/postgresql/
+  make_sure_postgresql_running
+  docker-compose exec -T postgresql touch /tmp/touch_me_to_promote_to_me_master
+  docker-compose exec -T postgresql ls /tmp/touch_me_to_promote_to_me_master
+  popd
+  touch /tmp/keepalived_is_masteer
+}
+
+
+become_backup(){
+  sleep 2 # wait until master ready
+  /opt/postgresql/become_standby.sh
+
+  rm -rf /tmp/keepalived_is_masteer
+  if [ -s /opt/harbor/docker-compose.yml ]; then
+      unhealthy_harbor_num=$(/bin/docker-compose -f /opt/harbor/docker-compose.yml -f /opt/harbor/docker-compose.clair.yml -f /opt/harbor/docker-compose.chartmuseum.yml ps | awk '/Exit|Restarting/' | wc -l)
+      if [ "$unhealthy_harbor_num" == "0" ]; then
+        /opt/harbor/install.sh --with-clair --with-chartmuseum
+      fi
+  fi
+}
+
 case $STATE in
     "MASTER")
-        echo "MASTER" | tee /tmp/keepalived_state
-        pushd /opt/postgresql/
-        make_sure_postgresql_running
-        docker-compose exec -T postgresql touch /tmp/touch_me_to_promote_to_me_master
-        docker-compose exec -T postgresql ls /tmp/touch_me_to_promote_to_me_master
-        popd
-        sleep 5
-        # pushd /opt/harbor/
-        # /opt/harbor/install.sh --with-clair --with-chartmuseum
-        # popd
+        become_master
         exit 0
         ;;
     "BACKUP")
-        echo "BACKUP" | tee /tmp/keepalived_state
-        sleep 60
-        # pushd /opt/harbor/
-        # /opt/harbor/install.sh --with-clair --with-chartmuseum
-        # popd
+        if [ -f /tmp/keepalived_is_masteer ] && [ -s "/data/database/PG_VERSION" ] ; then
+          become_backup
+        fi
         exit 0
         ;;
     "FAULT")
-        echo "FAULT" | tee /tmp/keepalived_state
-        #/opt/harbor/install.sh --with-clair --with-chartmuseum
+        if [ -f /tmp/keepalived_is_masteer ] && [ -s "/data/database/PG_VERSION" ] ; then
+          touch /tmp/disable_keepalived
+          become_backup
+          rm -rf /tmp/disable_keepalived
+        fi
         exit 0
         ;;
     *)
-        echo "unknown state"
+        echo "unknown state ${STATE}"
         exit 1
         ;;
 esac
